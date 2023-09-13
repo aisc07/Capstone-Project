2f9b6eb2f1d24c4e4f524134b2218316d2b432ce
aisc07 <amani.r.taylor@gmail.com>
2023-09-13 00:11:35 -0700

    updated css style

--- src/components/Cards/AllProducts.jsx
+++ src/components/Cards/AllProducts.jsx
@@ -4,23 +4,23 @@ import AllProductsCard from './AllProductsCard';
 function AllProducts() { 
   const [allProducts, setAllProducts] = useState([]);
       function fetchAllProducts(){
-         fetch("https://fakestoreapi.com/products")
-          .then((response) => response.json())
-          .then((data) => {
-           console.log(data)
-          setAllProducts(data)
-          });
-          
-      }
-        useEffect(()=>{fetchAllProducts()}
-          ,[]);
+          fetch("https://fakestoreapi.com/products")
+              .then((response) => response.json())
+              .then((data) => {
+                    console.log(data)
+                    setAllProducts(data)
+                  });
+                };
+        useEffect(()=>{
+        fetchAllProducts()
+      } 
+  ,[]);
    
   return (
     <div>
     {/* <h2>hi</h2> */}
         {allProducts?.map(product =><AllProductsCard key={product.id} product={product}/>)}
-        </div>
-        // return musicData.map(music=><MusicCard music={music}/>)
+    </div>
  
   )
 }
--- src/components/Cards/AllProductsCard.jsx
+++ src/components/Cards/AllProductsCard.jsx
@@ -4,15 +4,29 @@ import Card from 'react-bootstrap/Card';
 export default function AllProductsCard({product}){
        console.log(product)
     return(
-       
-       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={product.image}/>
-      
+        <div className="row">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+    <Card >
+        <Card.Img variant="top"  src={product.image}/>
+        {/* height="500" */}
       <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Header>{product.price}</Card.Header>
+        <Card.Title>{"$"}{product.price}</Card.Title>
+        <Card.Subtitle>{product.title}</Card.Subtitle>
+        <br/>
+        {/* <Card.Header>{"Rating: "}{product.rating.rate}</Card.Header> */}
+        {/* <Card.Footer>{"Count: "}{product.rating.count}</Card.Footer> */}
         <Card.Text>{product.description}</Card.Text>
         <Button variant="primary">See more</Button>
       </Card.Body>
     </Card>
+          </div>
+
+        </div>
+      // <div className="all">
+        // {/* style={{ width: '18rem' }} */}
+        
+    
+      // </div>
+       
+      
 )};
